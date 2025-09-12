0d28a35d98493a725b1b0458e7243cd92433d096
Elizabeth Cuevas <qevas@outlook.com>
2025-09-12 09:09:13 -0600

    feat: Implement principal display with video background and custom fonts

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,11 +1,23 @@
 <?php require_once 'partials/header.partial.php'; ?>
 
+    <div class="image-overlay">
+            <img class="foreground-image" src="assets/backgrounds/main-backgroundNF.png">
+    </div>
+
     <section id="section-one" class="fullscreen-section">
-     
+    <div class="video-background">
+        <video autoplay muted loop playsinline>
+            <source src="assets/videos/FIFA2026.mp4" type="video/mp4">
+        </video>
+    </div>    
+    <div class="title-content">
+            <h1>WORLD CUP LEGACY HUB</h1>
+            <h1>2026</h1>
+        </div>
     </section>
 
     <section id="section-two" class="fullscreen-section">
-    <div class="worldcup-cards-container">
+    <!-- <div class="worldcup-cards-container">
         <div class="card-wrapper">
             <p class="card-year-vertical">1930</p>
             <div class="worldcup-card">
@@ -19,11 +31,24 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
+
     </section>
 
     <section id="section-three" class="fullscreen-section">
-        <h2>Noticias</h2>
+        <div class="rows-wrapper">
+        <div class="card-row row-1">
+        <?php for ($i = 0; $i < 16; $i++): ?>
+            <div class="card-placeholder color-<?php echo ($i % 5) + 1; ?>"></div>
+        <?php endfor; ?>
+        </div>
+
+        <div class="card-row row-2">
+            <?php for ($i = 0; $i < 16; $i++): ?>
+                <div class="card-placeholder color-<?php echo ($i % 5) + 1; ?>"></div>
+            <?php endfor; ?>
+        </div>
+    </div> 
     </section>
 
         <section id="section-four" class="fullscreen-section">
